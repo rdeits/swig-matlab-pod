340e6e071c6293e8e687959652341551175e6a86
Robin Deits <robin.deits@gmail.com>
2015-12-22 01:12:34 -0500

    add prereq script

---
+++ install_prereqs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+case $1 in
+  ("homebrew")
+    brew install autoconf automake libtool ;;
+  ("macports")
+    port install automake autoconf libtool;;
+  ("ubuntu")
+    apt-get install build-essential ;;
+  ("cygwin")
+    cygwin-setup -q -P make autoconf automake libtool ;;
+  (*)
+    echo "Usage: ./install_prereqs.sh package_manager"
+    echo "where package_manager is one of the following: "
+    echo "  homebrew"
+    echo "  macports"
+    echo "  ubuntu"
+    echo "  cygwin"
+    exit 1 ;;
+esac
